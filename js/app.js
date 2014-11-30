832c1df37924aaed8f32630685d12e154ca12ea1
John Chen <cgg5207@sina.com>
2014-11-30 19:02:49 +0800

    百度天气api

--- js/app.js
+++ js/app.js
@@ -2,10 +2,9 @@ angular.module('myApp', ['ngRoute'])
 .provider('Weather', function() {
   var apiKey = "";
  
-  this.getUrl = function(type, ext) {
-    return "http://api.wunderground.com/api/" + 
-      this.apiKey + "/" + type + "/q/" +
-      ext + '.json';
+  this.getUrl = function(location) {
+    return "http://api.map.baidu.com/telematics/v3/weather?output=json&"+
+           "location="+location+"&"+"ak="+this.apiKey;
   };
  
   this.setApiKey = function(key) {
@@ -19,10 +18,10 @@ angular.module('myApp', ['ngRoute'])
         var d = $q.defer();
         $http({
           method: 'GET',
-          url: self.getUrl("forecast", city),
+          url: self.getUrl(city),
           cache: true
         }).success(function(data) {
-          d.resolve(data.forecast.simpleforecast);
+          d.resolve(data.results);
         }).error(function(err) {
           d.reject(err);
         });
@@ -46,7 +45,7 @@ angular.module('myApp', ['ngRoute'])
 })
 .factory('UserService', function() {
   var defaults = {
-    location: 'autoip'
+    location: '上海'
   };
  
   var service = {
@@ -66,7 +65,7 @@ angular.module('myApp', ['ngRoute'])
   return service;
 })
 .config(function(WeatherProvider) {
-  WeatherProvider.setApiKey('92b1f386ae6e6953');
+  WeatherProvider.setApiKey('xO5bpyIU3qIfAx2zLQIq5Fdf');
 })
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider
@@ -80,61 +79,6 @@ angular.module('myApp', ['ngRoute'])
     })
     .otherwise({redirectTo: '/'});
 }])
-.directive('autoFill', function($timeout, Weather) {
-  return {
-    restrict: 'EA',
-    scope: {
-      autoFill: '&',
-      ngModel: '=',
-      timezone: '='
-    },
-    compile: function(tEle, tAttrs) {
-      var tplEl = angular.element('<div class="typeahead">' +
-      '<input type="text" autocomplete="off" />' +
-      '<ul id="autolist" ng-show="reslist">' +
-        '<li ng-repeat="res in reslist" ' +
-          '>{{res.name}}</li>' +
-      '</ul>' +
-      '</div>');
-      var input = tplEl.find('input');
-      input.attr('type', tAttrs.type);
-      input.attr('ng-model', tAttrs.ngModel);
-      input.attr('timezone', tAttrs.timezone);
-      tEle.replaceWith(tplEl);
-      return function(scope, ele, attrs, ctrl) {
-        var minKeyCount = attrs.minKeyCount || 3,
-            timer;
- 
-        ele.bind('keyup', function(e) {
-          val = ele.val();
-          if (val.length < minKeyCount) {
-            if (timer) $timeout.cancel(timer);
-            scope.reslist = null;
-            return;
-          } else {
-            if (timer) $timeout.cancel(timer);
-            timer = $timeout(function() {
-              scope.autoFill()(val)
-              .then(function(data) {
-                if (data && data.length > 0) {
-                  scope.reslist = data;
-                  scope.ngModel = data[0].zmw;
-                  scope.timezone = data[0].tz;
-                }
-              });
-            }, 300);
-          }
-        });
- 
-        // Hide the reslist on blur
-        input.bind('blur', function(e) {
-          scope.reslist = null;
-          scope.$digest();
-        });
-      }
-    }
-  }
-})
 .controller('MainCtrl', 
   function($scope, $timeout, Weather, UserService) {
     $scope.date = {};
